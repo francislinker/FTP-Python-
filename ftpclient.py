4e00acbebd1b2337b9be6da392d35a4c89206a12
francis <15011053797@163.com>
2019-01-21 16:35:02 +0800

    end

--- ftpclient.py
+++ ftpclient.py
@@ -2,7 +2,67 @@ from socket import *
 import sys
 
 class ftpclient(object):
-    pass
+
+    def   __init__(self,client):
+        self.client = client
+
+    def dolist(self):
+        #向服务端发送请求，接收服务端消息
+        self.client.send(b'L')
+        data = self.client.recv(1024).decode()
+        if data == 'OK':
+            while True:
+                filename = self.client.recv(1024).decode()
+                if filename == '##':
+                    break
+                print('\033[32m'+filename+'\033[0m')
+        else:
+            print(data)
+
+    def doget(self):
+        filename = input('请输入要下载的文件名：')
+        message = 'G ' + filename
+        self.client.send(message.encode())
+        data = self.client.recv(1024).decode()
+        if data == 'OK':
+            f = open(filename,'wb')
+            while True:
+                data = self.client.recv(1024)
+                if data == b'##':
+                    break
+                f.write(data)
+            f.close()
+            print('%s 下载完成' % filename)
+        else:
+            print(data)
+            
+
+    def doput(self):
+        filename = input('请输入要上传的文件：')
+        filename2 = filename.split('/')[-1]
+        try:
+            f = open(filename,'rb')
+        except:
+            print('没有这个文件')
+            return
+
+        self.client.send(('P '+filename2).encode())
+        data = self.client.recv(1024)
+        if data == b'OK':
+            while True:
+                data = f.read(1024)
+                if not data:
+                    import time
+                    time.sleep(0.1)
+                    self.client.send(b'##')
+                    break
+                self.client.send(data)
+            f.close()
+            print('%s 上传完成' % filename2)
+
+    def doexit(self):
+        self.client.send(b'Q')
+        sys.exit('谢谢使用')
 
 def main():
     if len(sys.argv)<3:
@@ -16,13 +76,26 @@ def main():
         print("服务器连接失败！",e)
         return
     while True:
+        #创建对象，调用类内的方法
+        clientobj = ftpclient(client)
         print("============我的网盘===========")
         print("***    1.查看文件列表       *****")
-        print("***    1.下载文件       *****")
-        print("***    1.上传文件       *****")
-        print("***    1.退出网盘       *****")
+        print("***    2.下载文件       *****")
+        print("***    3.上传文件       *****")
+        print("***    4.退出网盘       *****")
         print("==============================")
         cmd = input('请做出选择（1/2/3/4')
+        if cmd.strip() in ['1','2','3','4']:
+            if cmd == '1':
+                clientobj.dolist()
+            elif cmd == '2':
+                clientobj.doget()
+            elif cmd == '3':
+                clientobj.doput()
+            else:
+                clientobj.doexit()
+        else:
+            print('请做出正确的选择（1/2/3/4)')
         
 if __name__ == "__main__":
     main()
--- ftpserver.py
+++ ftpserver.py
@@ -2,14 +2,87 @@ from socket import *
 import sys
 import os
 from threading import Thread
+import time
 
 address = ('0.0.0.0',8888)
+filedir = '/home/tarena/project/download/'
 
 def doRequest(client):
-    pass
+    #创建对象，每个客户端都有一个单独的对象来处理功能
+    serverobj = ftpserver(client)
+    #接收客户端的请求
+    while True:
+        message = client.recv(1024).decode()
+        msglist = message.split(' ')
+        if msglist[0] == 'L':
+            serverobj.dolist()
+        if msglist[0] == 'G':
+            serverobj.doget(msglist[-1])
+        if msglist[0] == 'P':
+            serverobj.doput(msglist[-1])
+        elif msglist[0] == 'Q':
+            serverobj.doexit()
 
 class ftpserver(object):
-    pass
+    def __init__(self,client):
+        self.client = client
+
+    def dolist(self):
+        
+        filelist = os.listdir(filedir)
+        if not filelist:
+            self.client.send('文件库为空'.encode())
+            
+            time.sleep(0.1)
+        else:
+            self.client.send(b'OK')
+            #发送文件名到客户端
+            for file in filelist:
+                #判断是否为普通文件
+                if os.path.isfile(filedir+file) and file[0] != '.':
+                    self.client.send(file.encode())
+                    time.sleep(0.1)
+
+            self.client.send(b'##')
+            time.sleep(0.1)
+
+    def doget(self,filename):
+        try:
+            f = open(filedir+filename,'rb')
+        except:
+            self.client.send('文件不存在'.encode())
+            return
+        
+        #文件已正常打开
+        self.client.send(b'OK')
+        time.sleep(0.1)
+        #发送文件的内容
+        while True:
+            data = f.read(1024)
+            if not data:
+                self.client.send(b'##')
+                time.sleep(0.1)
+                break
+            self.client.send(data)
+            #发送完毕后关闭文件
+            f.close()
+
+    def doput(self,filename):
+        try:
+            f = open(filedir+filename,'wb')
+        except:
+            self.client.send('上传失败'.encode())
+            return
+        self.client.send(b'OK')
+        while True:
+            data = self.client.recv(1024)
+            if data == b'##':
+                break
+            f.write(data)
+        f.close()
+
+    def doexit(self):
+        sys.exit(0)
 
 def main():
     server = socket(AF_INET,SOCK_STREAM)
